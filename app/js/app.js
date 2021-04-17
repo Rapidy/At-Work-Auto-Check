45708cc64d35429bb510d7192d20f51435d30728
Rapidy <artemchik03211@gmail.com>
2021-04-17 17:33:27 +0500

    0.4

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,34 @@
 // // Import jQuery module (npm i jquery)
-// import $ from 'jquery'
-// window.jQuery = $
-// window.$ = $
+import $ from 'jquery'
+window.jQuery = $
+window.$ = $
+
+import slick from 'slick-carousel'
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	$('img').attr('draggable','false');
+
+	$('.slider').slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		centerMode: true,
+		arrows: false,
+		centerPadding: '60px',
+	})
+
+	$('.slider-report').slick({
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		fade: true,
+		cssEase: 'linear',
+		speed: 500
+	})
 
 })
