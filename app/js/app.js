18076174c34ad6d61eec1a026d3101ed4c76856a
Rapidy <artemchik03211@gmail.com>
2021-04-22 18:24:08 +0500

    0.7

--- app/js/app.js
+++ app/js/app.js
@@ -12,24 +12,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
 	$('img').attr('draggable','false');
 
-	$('.slider').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		dots: true,
-		centerMode: true,
-		arrows: false,
-		autoplay: true,
-		autoplaySpeed: 4000
-	})
+	$('.checklist-nav__item').mouseenter(function() {
+		let currentNavItem = $(this).data('id');
+		let currentPhoto = $('.checklist-photo__item')[currentNavItem];
 
-	$('.slider-report').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 1,
-		fade: true,
-		cssEase: 'linear',
-		speed: 500
-	})
+		$('.checklist-photo__item').removeClass('active').addClass('hidden');
+		currentPhoto.classList.remove('hidden');
+		currentPhoto.classList.add('active')
+	});
+
+	$('.checklist-nav').mouseleave(function() {
+		$('.checklist-photo__item').addClass('hidden').removeClass('active');
+		$('.checklist-photo__item')[0].classList.add('active');
+		$('.checklist-photo__item')[0].classList.remove('hidden');
+	});
+
+	// $('.slider-report').slick({
+	// 	infinite: true,
+	// 	slidesToShow: 3,
+	// 	slidesToScroll: 1,
+	// 	fade: true,
+	// 	cssEase: 'linear',
+	// 	speed: 500
+	// })
 
 })
